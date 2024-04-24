987399060bd9aaf3c9af046d60b511d42f9110c8
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-04-24 18:06:21 +0300

    Fix Pearson consent criterion

--- include/statistics/dataVector/src/pearConsentCriterion.cpp
+++ include/statistics/dataVector/src/pearConsentCriterion.cpp
@@ -1,26 +1,30 @@
 #include "../dataVector.hpp"
 #include "classSeries.hpp"
+#include <cstdlib>
 
 double DataVector::pearConsentCriterion() {
-  if (rep.model == DistributionReproducer::Distribution::UnknownD)
-    return qQNaN();
+	if (rep.model == DistributionReproducer::Distribution::UnknownD)
+		return qQNaN();
 
-  if (stat.pearConsentCriterion.second == false)
-    computePearConsentCriterion();
+	if (stat.pearConsentCriterion.second == false)
+		computePearConsentCriterion();
 
-  return stat.pearConsentCriterion.first;
+	return stat.pearConsentCriterion.first;
 }
 
 void DataVector::computePearConsentCriterion() {
-  stat.pearConsentCriterion.first = 0;
-  for (int i = 0; i < cs->classCount(); i++) {
-    double ni = cs->series()[i].first,
-           nio = rep.cdf(min() + (i + 1) * (cs->step()));
-    nio -= rep.cdf(min() + i * (cs->step()));
-    nio *= size();
+	stat.pearConsentCriterion.first = 0;
+	const double k =  std::abs(rep.domain.first - rep.domain.second) /
+		std::abs(min() - max());
 
-    stat.pearConsentCriterion.first += std::pow(ni - nio, 2) / nio;
-  }
+	for (int i = 0; i < cs->classCount(); i++) {
+		double ni = cs->series()[i].first,
+			   nio = rep.cdf(rep.domain.first + (i + 1) * (cs->step()*k));
+		nio -= rep.cdf(rep.domain.first + i * (cs->step()*k));
+		nio *= size();
 
-  stat.pearConsentCriterion.second = true;
+		stat.pearConsentCriterion.first += std::pow(ni - nio, 2) / nio;
+	}
+
+	stat.pearConsentCriterion.second = true;
 }
