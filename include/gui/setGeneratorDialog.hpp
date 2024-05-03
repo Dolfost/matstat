f95880aae7c9d3b5a7bd56a2cff6aed5da2ece67
Vladyslav Rehan <rehanvladyslav@gmail.com>
2024-05-03 14:18:08 +0300

    Format code

--- include/gui/setGeneratorDialog.hpp
+++ include/gui/setGeneratorDialog.hpp
@@ -1,76 +1,72 @@
 #ifndef _SET_GENERATOR_DIALOG_
 #define _SET_GENERATOR_DIALOG_
 
-#include <QDialog>
-#include <QHBoxLayout>
-#include <QVBoxLayout>
-#include <QPushButton>
-#include <QLineEdit>
-#include <QLabel>
-#include <QGroupBox>
 #include <QComboBox>
+#include <QDialog>
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
 #include <QSpinBox>
 #include <QStatusBar>
+#include <QVBoxLayout>
 
 #include "guiTypes.hpp"
 
 class ParametersWidget : public QWidget {
-	Q_OBJECT
+  Q_OBJECT
 public:
-		ParametersWidget(QStringList, std::vector<double>, bool = true);
-		std::vector<double> parameters();
+  ParametersWidget(QStringList, std::vector<double>, bool = true);
+  std::vector<double> parameters();
 
 private:
-		QGridLayout* layout;
-		size_t size = 0;
-		int precision = 4;
+  QGridLayout *layout;
+  size_t size = 0;
+  int precision = 4;
 };
 
 class SetGeneratorDialog : public QDialog {
-	Q_OBJECT
+  Q_OBJECT
 public:
-	SetGeneratorDialog(
-			VectorEntry* = nullptr,
-			QWidget* = nullptr,
-			bool show = true,
-			Qt::WindowFlags = Qt::WindowFlags()
-			);
+  SetGeneratorDialog(VectorEntry * = nullptr, QWidget * = nullptr,
+                     bool show = true, Qt::WindowFlags = Qt::WindowFlags());
 
 private:
-	QComboBox* distributionComboBox = nullptr;
-	QGroupBox* boundsBox = nullptr;
-	QSpinBox* countSpinBox = nullptr;
-	QDoubleSpinBox* minSpinBox = nullptr;
-	QDoubleSpinBox* maxSpinBox = nullptr;
-	ParametersWidget* parametersWidget = nullptr;
-	QVBoxLayout* parametersLayout = nullptr;
-	QComboBox* methodComboBox = nullptr;
+  QComboBox *distributionComboBox = nullptr;
+  QGroupBox *boundsBox = nullptr;
+  QSpinBox *countSpinBox = nullptr;
+  QDoubleSpinBox *minSpinBox = nullptr;
+  QDoubleSpinBox *maxSpinBox = nullptr;
+  ParametersWidget *parametersWidget = nullptr;
+  QVBoxLayout *parametersLayout = nullptr;
+  QComboBox *methodComboBox = nullptr;
 
-	QPushButton* generateButton = nullptr;
+  QPushButton *generateButton = nullptr;
 
-	QStatusBar* statusBar = nullptr;
+  QStatusBar *statusBar = nullptr;
 
-	VectorEntry* ve = nullptr;
+  VectorEntry *ve = nullptr;
 
-	std::vector<double> params = {};
+  std::vector<double> params = {};
 
-	bool enabled = true;
+  bool enabled = true;
 
-	int precision = 4;
+  int precision = 4;
 
 public slots:
-	void vectorDeletedHandler(VectorEntry*);
+  void vectorDeletedHandler(VectorEntry *);
 
 private slots:
-	void generate();
-	void distributionSelected(int);
-	void methodSelected(int);
-	void minBoundChanged(double);
-	void maxBoundChanged(double);
+  void generate();
+  void distributionSelected(int);
+  void methodSelected(int);
+  void minBoundChanged(double);
+  void maxBoundChanged(double);
 
 signals:
-	void setGenerated(VectorEntry*);
-	void message(QString);
+  void setGenerated(VectorEntry *);
+  void message(QString);
 };
 
 #endif // !_SET_GENERATOR_DIALOG_
