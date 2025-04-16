181963bbc84c63f5de91e4312685275a00ee4975
Vladyslav Rehan <rehanvladyslav@gmail.com>
2025-04-16 20:54:22 +0300

    Fix plural corelation

--- src/statistics/vectorChain/src/partial_corelation.cpp
+++ src/statistics/vectorChain/src/partial_corelation.cpp
@@ -4,26 +4,25 @@
 
 namespace ss {
 
-inline double partialCorelationRatio(double r_i_j, double r_i_d, double r_j_d){
-	return (r_i_j - r_i_d*r_j_d)/std::sqrt((1-r_i_d*r_i_d)*(1 - r_j_d*r_j_d));
+inline double partialCorelationRatio(double r_ij, double r_id, double r_jd){
+	return (r_ij - r_id*r_jd)/std::sqrt((1-r_id*r_id)*(1 - r_jd*r_jd));
 }
 
 double rec(VectorChain* vc, std::size_t i, std::size_t j, std::vector<std::size_t> c, std::size_t d) {
-    if (c.size() == 0) {
-        return partialCorelationRatio((*vc)[i][j], (*vc)[i][d], (*vc)[j][d]);
-    }
+	if (c.size() == 0)
+		return partialCorelationRatio((*vc).r()[i][j], (*vc).r()[i][d], (*vc).r()[j][d]);
 
-    int newD = c.back();
-    c.pop_back();
+	int newD = c.back();
+	c.pop_back();
 
-    double first = rec(vc, i, j, c, newD);
-    double second = rec(vc, i, d, c, newD);
-    double third = rec(vc, j, d, c, newD);
+	double first = rec(vc, i, j, c, newD);
+	double second = rec(vc, i, d, c, newD);
+	double third = rec(vc, j, d, c, newD);
 
-    double numerator = first - second*third;
-    double denominator = std::sqrt(std::abs((1 - std::pow(second, 2)) * (1 - std::pow(third, 2))));
+	double numerator = first - second*third;
+	double denominator = std::sqrt(std::abs((1 - std::pow(second, 2)) * (1 - std::pow(third, 2))));
 
-    return numerator / denominator;
+	return numerator / denominator;
 }
 
 void VectorChain::PartialCorelationRatio::adapt(std::tuple<std::size_t, std::size_t, std::vector<std::size_t>, std::size_t> t) {
